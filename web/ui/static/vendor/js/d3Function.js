7610ffcfd454b121c58d475114527e57a641a383
prk327 <prk327@gmail.com>
2020-05-31 20:10:34 +0530

    modifyed some bootstrat 4

--- web/ui/static/vendor/js/d3Function.js
+++ web/ui/static/vendor/js/d3Function.js
@@ -1,35 +1,47 @@
-const data = [
-    {
-        width: 200,
-        height: 100,
-        fill: 'purple'
-    },
-    {
-        width: 100,
-        height: 60,
-        fill: 'pink'
-    },
-    {
-        width: 50,
-        height: 30,
-        fill: 'red'
-    }
-];
+// const data = [
+//     {
+//         width: 200,
+//         height: 100,
+//         fill: 'purple'
+//     },
+//     {
+//         width: 100,
+//         height: 60,
+//         fill: 'pink'
+//     },
+//     {
+//         width: 50,
+//         height: 30,
+//         fill: 'red'
+//     }
+// ];
 
-const svg = d3.select('svg');
+// const svg = d3.select('svg');
 
-//Join data to rects
-const rects = svg.selectAll('rect')
-    .data(data);
+// //Join data to rects
+// const rects = svg.selectAll('rect')
+//     .data(data);
 
-//add attrs to rects already in DOM
-rects.attr('width', d => d.width)
-    .attr('height', d => d.height)
-    .attr('fill', d => d.fill);
+// //add attrs to rects already in DOM
+// rects.attr('width', d => d.width)
+//     .attr('height', d => d.height)
+//     .attr('fill', d => d.fill);
 
-//append the enter selection to DOM
-rects.enter()
-    .append('rect')
-    .attr('width', d => d.width)
-    .attr('height', d => d.height)
-    .attr('fill', d => d.fill);
+// //append the enter selection to DOM
+// rects.enter()
+//     .append('rect')
+//     .attr('width', d => d.width)
+//     .attr('height', d => d.height)
+//     .attr('fill', d => d.fill);
+
+// $(start);
+
+$(function(){
+    document.querySelector('#webgl').innerHTML = new Date;
+    console.log("This is not working")
+  });
+
+// function start (){
+//     console.log("I am started!")
+// }
+
